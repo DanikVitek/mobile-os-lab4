440e27c1bc1f89f3947536161dea9036134d6ecc
Danik Vitek <x3665107@gmail.com>
2024-09-19 13:03:34 +0300

    Initial commit

---
+++ app/src/main/java/me/danikvitek/lab4/Lab4App.kt
@@ -0,0 +1,7 @@
+package me.danikvitek.lab4
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Lab4App : Application()
---
+++ app/src/main/java/me/danikvitek/lab4/MainActivity.kt
@@ -0,0 +1,46 @@
+package me.danikvitek.lab4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.EntryPoint
+import kotlinx.serialization.Serializable
+import me.danikvitek.lab4.ui.theme.Lab4Theme
+
+@Serializable
+private object StartScreen
+
+@EntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab4Theme {
+                val navController = rememberNavController()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = StartScreen,
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable<StartScreen> {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
